4d4bebebb428105c5911b96a72e33f555cdaa586
Heiko Braun <ike.braun@googlemail.com>
2017-01-30 14:07:11 +0100

    cleanup

--- scripts/token_req.sh
+++ scripts/token_req.sh
@@ -4,14 +4,14 @@ SCRIPT_DIR="$(dirname "$0")"
 
 . $SCRIPT_DIR/common.sh
 
-echo ">>> HTTP Token query"
-echo "curl -sk -X POST $SSO_HOST/auth/realms/$REALM/protocol/openid-connect/token -d grant_type=password -d username=$USER -d client_secret=$SECRET -d password=$PASSWORD -d client_id=$CLIENT_ID"
+#echo ">>> HTTP Token query"
+#echo "curl -skv -X POST $SSO_HOST/auth/realms/$REALM/protocol/openid-connect/token -d grant_type=password -d username=$USER -d client_secret=$SECRET -d password=$PASSWORD -d client_id=$CLIENT_ID"
 
 auth_result=$(curl -sk -X POST $SSO_HOST/auth/realms/$REALM/protocol/openid-connect/token -d grant_type=password -d username=$USER -d client_secret=$SECRET -d password=$PASSWORD -d client_id=$CLIENT_ID)
 access_token=$(echo -e "$auth_result" | awk -F"," '{print $1}' | awk -F":" '{print $2}' | sed s/\"//g | tr -d ' ')
 
-echo ">>> TOKEN Received"
-echo $access_token
+#echo ">>> TOKEN Received"
+#echo $access_token
 
 echo ">>> Greeting"
 curl -kv $APP/greeting -H "Authorization:Bearer $access_token"
