33964ff17b3bdec410d66c20a72498be3901a41c
Heiko Braun <ike.braun@googlemail.com>
2017-01-30 13:38:21 +0100

    Added testing scripts

---
+++ scripts/common.sh
@@ -0,0 +1,13 @@
+REALM=master
+USER=admin
+PASSWORD=admin
+CLIENT_ID=demoapp
+SECRET=cb7a8528-ad53-4b2e-afb8-72e9795c27c8
+SSO_HOST=${1:-https://secure-sso-obsidian.e8ca.engint.openshiftapps.com}
+APP=${2:-http://swarm-secured-rest-obsidian.e8ca.engint.openshiftapps.com/greeting}
+
+function jsonValue() {
+  KEY=$1
+  num=$2
+  awk -F"[,:}]" '{for(i=1;i<=NF;i++){if($i~/'$KEY'\042/){print $(i+1)}}}' | tr -d '"' | sed -n ${num}p
+}
