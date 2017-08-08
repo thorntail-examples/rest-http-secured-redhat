9c77aa2779df9df0e0ec88d8c4db35a0af5a29fd
Ladislav Thon <ladicek@gmail.com>
2017-08-08 16:33:30 +0200

    update the Swarm SSO booster (#23)
    This commit updates:
    
    - WildFly Swarm
    - booster parent
    - SSO Git submodule
    
    Each to the respective latest version.
    
    It also adds a demo webpage, cleans up the POMs, and moves
    from `org.obsidiantoaster.quickstart` to `io.openshift.booster`.

--- app/src/main/java/io/openshift/booster/GreetingController.java
+++ app/src/main/java/io/openshift/booster/GreetingController.java
@@ -13,7 +13,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-package org.obsidiantoaster.quickstart.sso;
+package io.openshift.booster;
 
 import java.util.concurrent.atomic.AtomicLong;
 
--- app/src/test/java/io/openshift/booster/GreetingServiceTest.java
+++ app/src/test/java/io/openshift/booster/GreetingServiceTest.java
@@ -14,7 +14,7 @@
  *  limitations under the License.
  */
 
-package org.obsidiantoaster.quickstart.sso;
+package io.openshift.booster;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
@@ -22,20 +22,12 @@ import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.shrinkwrap.api.Archive;
-import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.obsidiantoaster.quickstart.sso.GreetingController;
-import org.wildfly.swarm.Swarm;
-import org.wildfly.swarm.arquillian.CreateSwarm;
 import org.wildfly.swarm.arquillian.DefaultDeployment;
-import org.wildfly.swarm.jaxrs.JAXRSArchive;
-import org.wildfly.swarm.jaxrs.JAXRSFraction;
 
 /**
  * @author Heiko Braun
@@ -48,12 +40,14 @@ public class GreetingServiceTest {
     @RunAsClient
     public void testResource() {
         Client client = ClientBuilder.newClient();
-        WebTarget target = client.target("http://localhost:8080")
-                .path("greeting");
-
-        Response response = target.request(MediaType.APPLICATION_JSON).get();
-        Assert.assertEquals(200, response.getStatus());
-        Assert.assertTrue(response.readEntity(String.class).contains("Hello, World!"));
+        try {
+            WebTarget target = client.target("http://localhost:8080").path("api").path("greeting");
+            Response response = target.request(MediaType.APPLICATION_JSON).get();
+            Assert.assertEquals(200, response.getStatus());
+            Assert.assertTrue(response.readEntity(String.class).contains("Hello, World!"));
+        } finally {
+            client.close();
+        }
     }
 
 }
